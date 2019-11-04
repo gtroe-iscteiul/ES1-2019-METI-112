c1056afa4e6124cfd08ad65e29abf352ec4bc783
gtroe-iscteiul <gtroeiscte-iul.pt>
2019-11-04 20:27:00 +0000

    Adding Maven Project

---
+++ ES_2019_2020_Project/src/main/java/ES1_2019_METI_112/ES_2019_2020_Project/App.java
@@ -0,0 +1,13 @@
+package ES1_2019_METI_112.ES_2019_2020_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
