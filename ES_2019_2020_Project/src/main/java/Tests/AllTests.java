add375e6e392e14768bd59a9286f8f5b5bc51763
gtroe-iscteiul <gtroeiscte-iul.pt>
2019-12-13 23:07:51 +0000

    correcção no alltests

--- ES_2019_2020_Project/src/main/java/Tests/AllTests.java
+++ ES_2019_2020_Project/src/main/java/Tests/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;;
 
 @RunWith(JUnitPlatform.class)
-@SelectClasses({AccessToRuleDatabaseTest.class, FileHandlingTest.class, GUI_Defect_Detection_JTableTest.class, GUI_Defect_Detection_RuleTest.class,
+@SelectClasses({AccessToRuleDatabaseTest.class, FileHandlingTest.class, GUI_Defect_Detection_RuleTest.class,
 	GUI_Defect_DetectionTest.class, GUI_Main_ClassTest.class, GUI_Operative_FrameTest.class, GUI_Rule_Frame_Choosing_Consequence_TypeTest.class,
 	GUI_Rule_Frame_Creating_Feature_Envy_ConditionTest.class, GUI_Rule_Frame_Creating_Feature_Envy_ConsequenceTest.class, 
 	GUI_Rule_Frame_Creating_Feature_Envy_Final_ResultTest.class, GUI_Rule_Frame_Creating_Long_MethodTest.class, GUI_Rules_FrameTest.class,
